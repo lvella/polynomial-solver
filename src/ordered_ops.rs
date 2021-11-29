3f8e19df1b7103a79fc965f6eb6ad99bf865b5e3
Lucas Clemente Vella <lucas.vella@ethereum.org>
2021-11-29 20:54:42 +0000

    Added missing file.

---
+++ src/ordered_ops.rs
@@ -0,0 +1,115 @@
+/// A term of the summable sequence with total order for the key, and
+/// a summable type with null value.
+/// In a polynomial term, Key is the monomial, and Value is the coefficient.
+/// In a monomial term, Key is the variable, and Value is the power.
+pub trait Term {
+    type Key;
+    type Value;
+
+    fn key(&self) -> &Self::Key;
+    fn value(self) -> Self::Value;
+    fn value_ref(&self) -> &Self::Value;
+    fn value_ref_mut(&mut self) -> &mut Self::Value;
+}
+
+/// Implements ordered sum, when sum depends on summing two ordered sequence.
+/// Used to multiply two monomials or sum two polynomials.
+pub fn sum<T>(
+    mut a_iter: impl Iterator<Item = T>,
+    mut b_iter: impl Iterator<Item = T>,
+    cmp: impl Fn(&T, &T) -> std::cmp::Ordering,
+    op: impl Fn(T, T) -> Option<T>,
+) -> Vec<T> {
+    let mut new_terms = Vec::new();
+
+    let mut a = a_iter.next();
+    let mut b = b_iter.next();
+
+    loop {
+        match (a, b) {
+            (Some(mut va), Some(mut vb)) => match cmp(&va, &vb) {
+                std::cmp::Ordering::Equal => {
+                    // Do the operation
+                    if let Some(r) = op(va, vb) {
+                        new_terms.push(r);
+                    }
+                    a = a_iter.next();
+                    b = b_iter.next();
+                }
+                std::cmp::Ordering::Less => {
+                    new_terms.push(va);
+                    a = a_iter.next();
+                    b = Some(vb);
+                }
+                std::cmp::Ordering::Greater => {
+                    new_terms.push(vb);
+                    a = Some(va);
+                    b = b_iter.next();
+                }
+            },
+            (None, Some(b)) => {
+                new_terms.push(b);
+                new_terms.extend(b_iter);
+                break;
+            }
+            (Some(a), None) => {
+                new_terms.push(a);
+                new_terms.extend(a_iter);
+                break;
+            }
+            (None, None) => {
+                break;
+            }
+        }
+    }
+
+    new_terms
+}
+
+/// Implements ordered saturated difference.
+/// Used to calculate the complementing monomial in spar algorithm
+pub fn saturating_sub<T, U>(
+    mut a_iter: impl Iterator<Item = T>,
+    mut b_iter: impl Iterator<Item = U>,
+    cmp: impl Fn(&T, &U) -> std::cmp::Ordering,
+    op: impl Fn(T, U) -> Option<T>,
+) -> Vec<T> {
+    let mut new_terms = Vec::new();
+
+    let mut a = a_iter.next();
+    let mut b = b_iter.next();
+
+    loop {
+        match (a, b) {
+            (Some(mut va), Some(mut vb)) => match cmp(&va, &vb) {
+                std::cmp::Ordering::Equal => {
+                    // Do the operation
+                    if let Some(r) = op(va, vb) {
+                        new_terms.push(r);
+                    }
+                    a = a_iter.next();
+                    b = b_iter.next();
+                }
+                std::cmp::Ordering::Less => {
+                    new_terms.push(va);
+                    a = a_iter.next();
+                    b = Some(vb);
+                }
+                std::cmp::Ordering::Greater => {
+                    a = Some(va);
+                    b = b_iter.next();
+                }
+            },
+            (Some(a), None) => {
+                new_terms.push(a);
+                new_terms.extend(a_iter);
+                break;
+            }
+            _ => {
+                break;
+            }
+        }
+    }
+
+    new_terms
+}
