d60cd4869780bc1f6aa6bd38103f06486d77871d
Lucas Clemente Vella <lucas.vella@ethereum.org>
2022-11-25 14:40:58 +0000

    Reorganized benchmark execution.

--- benchmarks/run_benchmarks.py
+++ benchmarks/run_benchmarks.py
@@ -1,35 +1,50 @@
 #!/usr/bin/env python3
 
 # Benchmarks to run from ZoKrates files:
+import datetime
+import subprocess
+import os
 zokrates_benchmarks = ['u8_different_from_zero.zok_bin']
 
 # Benchmarks to run based from Maple-like files:
 maple_like_benchmarks = [('benchmark.txt', 20)]
 
-import os
-import subprocess
-import datetime
+
+timeout = 60*5  # 5 minutes
+
+
+def gb_run(particular_args, output_file):
+    stdout = open(f'{output_file}.out', 'wb')
+    stderr = open(f'{output_file}.err', 'wb')
+
+    cmd = ['../target/release/grobner-basis', '-v',
+           f'{output_file}.verification.cocoa5'] + particular_args
+    sub = subprocess.Popen(cmd, stdout=stdout, stderr=stderr)
+
+    try:
+        sub.wait(timeout)
+        print('finished!')
+    except subprocess.TimeoutExpired:
+        sub.kill()
+        print('timed out!')
+
 
 def main():
-    out_dir = f'run_{datetime.datetime.utcnow()}'
+    out_dir = f'{datetime.datetime.utcnow()}-run'
     os.mkdir(out_dir)
 
     # Run ZoKrates benchmarks
     for f in zokrates_benchmarks:
         print(f'Running ZoKrates benchmark "{f}"...')
-        stdout = open(f'{out_dir}/{f}.out', 'wb')
-        stderr = open(f'{out_dir}/{f}.err', 'wb')
-
-        subprocess.run(['../target/release/polysolver', '-z', f, '-v', f'{out_dir}/{f}.verification.cocoa5'], stdout=stdout, stderr=stderr)
+        gb_run(['-z', f'inputs/{f}'], f'{out_dir}/{f}')
 
     # Run Maple-like benchmarks
     for (f, count) in maple_like_benchmarks:
         for idx in range(count):
             print(f'Running Maple-like benchmark {idx} from "{f}"...')
-            stdout = open(f'{out_dir}/{f}.{idx}.out', 'wb')
-            stderr = open(f'{out_dir}/{f}.{idx}.err', 'wb')
+            gb_run(['-m', f'inputs/{f}', '-i', str(idx)],
+                   f'{out_dir}/{f}.{idx}')
 
-            subprocess.run(['../target/release/polysolver', '-m', f, '-i', str(idx), '-v', f'{out_dir}/{f}.{idx}.verification.cocoa5'], stdout=stdout, stderr=stderr)
 
 if __name__ == '__main__':
     main()
